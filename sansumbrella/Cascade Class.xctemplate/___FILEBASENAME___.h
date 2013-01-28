2ff229462e42734f82b61e344dd9a0625c95e6ca
David Wicks <david@sansumbrella.com>
2013-01-28 15:32:20 -0500

    Personal xcode templates added.

---
+++ sansumbrella/Cascade Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#pragma once
+
+namespace cascade
+{
+	class ___FILEBASENAME___
+	{
+	public:
+		___FILEBASENAME___();
+		~___FILEBASENAME___();
+	private:
+	};
+}
